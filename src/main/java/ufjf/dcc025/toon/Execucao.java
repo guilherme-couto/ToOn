704dfc9ddba85420df30a69583604058f7f8808e
Guilherme C <62406808+guilherme-couto@users.noreply.github.com>
2021-03-01 16:57:05 -0300

    Add Arquivos Iniciais

---
+++ src/main/java/ufjf/dcc025/toon/Execucao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ufjf.dcc025.toon;
+
+/**
+ *
+ * @author guilherme
+ */
+public class Execucao {
+    public static void main(String[] args) {
+        System.out.println("FIM");
+    }
+}
